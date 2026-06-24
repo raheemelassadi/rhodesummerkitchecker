cc3838687c0eb26cf658bf03a30e7135dc6e83cd
raheemelassadi <raheemelassadi1@gmail.com>
2026-06-23 22:56:00 -0700

    completed website

--- rhodesummerkitchecker/vite.config.js
+++ rhodesummerkitchecker/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/rhodesummerkitchecker/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
@@ -14,3 +15,6 @@ export default defineConfig({
     },
   },
 })
+
+
+
